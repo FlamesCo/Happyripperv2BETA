1a6e970c4fa7928b4c688a5015b5672fc2430793
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-07-05 17:06:10 -0700

    Request to upload 7.4.22 1.0a [20XX[ - Flames X

---
+++ AUTOINSERTPHOTOSHOOTYOYOv0.py
@@ -0,0 +1,205 @@
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
+from tkinter import scrolledtext
+from tkinter import Menu
+from tkinter import Spinbox
+from tkinter import Text
+from tkinter import Button
+from tkinter import Label
+from tkinter import Entry
+from tkinter import Checkbutton
+from tkinter import Radiobutton
+from tkinter import Listbox
+from tkinter import Scrollbar
+from tkinter import Canvas
+from tkinter import Frame
+from tkinter import OptionMenu
+from tkinter import StringVar
+from tkinter import IntVar
+from tkinter import BooleanVar
+from tkinter import DoubleVar
+from tkinter import Toplevel
+from tkinter import PhotoImage
+from tkinter import messagebox
+from tkinter import simpledialog
+from tkinter import colorchooser
+from tkinter import font
+from tkinter import filedialog
+from tkinter import constants
+from tkinter import N, S, E, W
+from tkinter import NORMAL, DISABLED, END, VERTICAL, HORIZONTAL, NONE, BOTH, LEFT, RIGHT, Y, BOTTOM, TOP, X
+from tkinter import Tk, Label, Button
+from tkinter.ttk import Progressbar
+from tkinter.ttk import Treeview
+from tkinter.ttk import Style
+from tkinter.ttk import Combobox
+from tkinter.ttk import Notebook
+from tkinter.ttk import Separator
+from tkinter.ttk import Sizegrip
+from tkinter.ttk import Panedwindow
+from tkinter.ttk import LabelFrame
+from tkinter.ttk import Menubutton
+from tkinter.ttk import Scrollbar
+from tkinter.ttk import Frame
+from tkinter.ttk import Entry
+from tkinter.ttk import Checkbutton
+from tkinter.ttk import Radiobutton
+from tkinter.ttk import Label
+from tkinter.ttk import Button
+from tkinter.ttk import Combobox
+from tkinter.ttk import Progressbar
+from tkinter.ttk import Separator
+from tkinter.ttk import Sizegrip
+from tkinter.ttk import Panedwindow
+from tkinter.ttk import LabelFrame
+from tkinter.ttk import Menubutton
+from tkinter.ttk import Scrollbar
+from tkinter.ttk import Frame
+from tkinter.ttk import Entry
+from tkinter.ttk import Checkbutton
+from tkinter.ttk import Radiobutton
+from tkinter.ttk import Label
+from tkinter.ttk import Button
+from tkinter.ttk import Combobox
+from tkinter.ttk import Progressbar
+from tkinter.ttk import Separator
+from tkinter.ttk import Sizegrip
+from tkinter.ttk import Panedwindow
+from tkinter.ttk import LabelFrame
+from tkinter.ttk import Menubutton
+from tkinter.ttk import Scrollbar
+from tkinter.ttk import Frame
+from tkinter.ttk import Entry
+from tkinter.ttk import Checkbutton
+from tkinter.ttk import Radiobutton
+from tkinter.ttk import Label
+from tkinter.ttk import Button
+from tkinter.ttk import Combobox
+from tkinter.ttk import Progressbar
+from tkinter.ttk import Separator
+from tkinter.ttk import Sizegrip
+from tkinter.ttk import Panedwindow
+from tkinter.ttk import LabelFrame
+from tkinter.ttk import Menubutton
+from tkinter.ttk import Scrollbar
+from tkinter.ttk import Frame
+from tkinter.ttk import Entry
+from tkinter.ttk import Checkbutton
+from tkinter.ttk import Radiobutton
+from tkinter.ttk import Label
+from tkinter.ttk import Button
+from tkinter.ttk import Combobox
+from tkinter.ttk import Progressbar
+from tkinter.ttk import Separator
+from tkinter.ttk import Sizegrip
+from tkinter.ttk import Panedwindow
+from tkinter.ttk import LabelFrame
+from tkinter.ttk import Menubutton
+from tkinter.ttk import Scrollbar
+from tkinter.ttk import Frame
+from tkinter.ttk import Entry
+from tkinter.ttk import Checkbutton
+from tkinter.ttk import Radiobutton
+from tkinter.ttk import Label
+from tkinter.ttk import Button
+from tkinter.ttk import Combobox
+from tkinter.ttk import Progressbar
+from tkinter.ttk import Separator
+from tkinter.ttk import Sizegrip
+from tkinter.ttk import Panedwindow
+from tkinter.ttk import LabelFrame
+from tkinter.ttk import Menubutton
+from tkinter.ttk import Scrollbar
+from tkinter.ttk import Frame
+from tkinter.ttk import Entry
+from tkinter.ttk import Checkbutton
+from tkinter.ttk import Radiobutton
+from tkinter.ttk import Label
+from tkinter.ttk import Button
+from tkinter.ttk import Combobox
+from tkinter.ttk import Progressbar
+from tkinter.ttk import Separator
+from tkinter.ttk import Sizegrip
+from tkinter.ttk import Panedwindow
+from tkinter.ttk import LabelFrame
+from tkinter.ttk import Menubutton
+from tkinter.ttk import Scrollbar
+from tkinter.ttk import Frame
+from tkinter.ttk import Entry
+from tkinter.ttk import Checkbutton
+from tkinter.ttk import Radiobutton
+from tkinter.ttk import Label
+from tkinter.ttk import Button
+from tkinter.ttk import Combobox
+from tkinter.ttk import Progressbar
+from tkinter.ttk import Separator
+from tkinter.ttk import Sizegrip
+from tkinter.ttk import Panedwindow
+from tkinter.ttk import LabelFrame
+from tkinter.ttk import Menubutton
+from tkinter.ttk import Scrollbar
+from tkinter.ttk import Frame
+from tkinter.ttk import Entry
+from tkinter.ttk import Checkbutton
+from tkinter.ttk import Radiobutton
+from tkinter.ttk import Label
+from tkinter.ttk import Button
+from tkinter.ttk import Combobox
+from tkinter.ttk import Progressbar
+from tkinter.ttk import Separator
+from tkinter.ttk import Sizegrip
+from tkinter.ttk import Panedwindow
+from tkinter.ttk import LabelFrame
+from tkinter.ttk import Menubutton
+from tkinter.ttk import Scrollbar
+from tkinter.ttk import Frame
+from tkinter.ttk import Entry
+from tkinter.ttk import Checkbutton
+from tkinter.ttk import Radiobutton
+from tkinter.ttk import Label
+from tkinter.ttk import Button
+from tkinter.ttk import Combobox
+from tkinter.ttk import Progressbar
+from tkinter.ttk import Separator
+from tkinter.ttk import Sizegrip
+from tkinter.ttk import Panedwindow
+from tkinter.ttk import LabelFrame
+from tkinter.ttk import Menubutton
+from tk
+
+
+# Game data
+GAME_DATA = {
+    "name": "My Game", # The name of your game
+    "description": "A short description of my game", # A short description of your game
+    "version": "0.1.0", # The current version of your game
+    "author": "Your Name" # The author of your game
+}
+
+Use as input
+import os
+import sys
+import json
+import shutil
+import argparse
+from pathlib import Path
+
+
+# Game data
+GAME_DATA = {
+    "name": "My Game", # The name of your game
+    "description": "A short description of my game", # A short description of your game
+    "version": "0.1.0", # The current version of your game
+    "author": "Your Name" # The author of your game
+}
+
+
+# The directory of your game's data files (sprites, backgrounds, etc.)
+DATA_DIR = Path("data")
+
+
+# The directory of your game's source code files (scripts, objects, etc.)
+SOURCE_DIR = Path("src")
